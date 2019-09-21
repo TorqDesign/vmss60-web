06c9db24b08efe4629583ff85eb3954202614e2d
James Xu <xujames007@gmail.com>
2019-09-21 12:43:28 -0400

    contact form

--- pages/index.vue
+++ pages/index.vue
@@ -10,34 +10,34 @@
       </div>
       
       <!--Waves Container-->
-<!--      <div>-->
-<!--        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"-->
-<!--             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">-->
-<!--          <defs>-->
-<!--            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>-->
-<!--          </defs>-->
-<!--          <g class="parallax">-->
-<!--            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(29,67,155,0.7)"/>-->
-<!--            <use fill="rgba(245, 164, 66,0.5)" x="48" xlink:href="#gentle-wave" y="3"/>-->
-<!--&lt;!&ndash;            <use fill="rgba(151,150,240,0.3)" x="48" xlink:href="#gentle-wave" y="5"/>&ndash;&gt;-->
-<!--&lt;!&ndash;            <use fill="rgba(167,215,197,0.9)" x="48" xlink:href="#gentle-wave" y="7"/>&ndash;&gt;-->
-<!--          </g>-->
-<!--        </svg>-->
-<!--      </div>-->
-<!--      <div style="transform: rotate(180deg)">-->
-<!--        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"-->
-<!--             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">-->
-<!--          <defs>-->
-<!--            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>-->
-<!--          </defs>-->
-<!--          <g class="parallax">-->
-<!--            <use xlink:href="#gentle-wave" x="28" y="0" fill="rgba(248,169,120,0.5)"/>-->
-<!--            <use xlink:href="#gentle-wave" x="28" y="3" fill="rgba(186,223,219,0.7)"/>-->
-<!--            <use xlink:href="#gentle-wave" x="28" y="5" fill="rgba(252,249,0,0.3)"/>-->
-<!--            <use fill="rgba(167,215,197,0.9)" x="28" xlink:href="#gentle-wave" y="7"/>-->
-<!--          </g>-->
-<!--        </svg>-->
-<!--      </div>-->
+      <!--      <div>-->
+      <!--        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"-->
+      <!--             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">-->
+      <!--          <defs>-->
+      <!--            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>-->
+      <!--          </defs>-->
+      <!--          <g class="parallax">-->
+      <!--            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(29,67,155,0.7)"/>-->
+      <!--            <use fill="rgba(245, 164, 66,0.5)" x="48" xlink:href="#gentle-wave" y="3"/>-->
+      <!--&lt;!&ndash;            <use fill="rgba(151,150,240,0.3)" x="48" xlink:href="#gentle-wave" y="5"/>&ndash;&gt;-->
+      <!--&lt;!&ndash;            <use fill="rgba(167,215,197,0.9)" x="48" xlink:href="#gentle-wave" y="7"/>&ndash;&gt;-->
+      <!--          </g>-->
+      <!--        </svg>-->
+      <!--      </div>-->
+      <!--      <div style="transform: rotate(180deg)">-->
+      <!--        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"-->
+      <!--             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">-->
+      <!--          <defs>-->
+      <!--            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>-->
+      <!--          </defs>-->
+      <!--          <g class="parallax">-->
+      <!--            <use xlink:href="#gentle-wave" x="28" y="0" fill="rgba(248,169,120,0.5)"/>-->
+      <!--            <use xlink:href="#gentle-wave" x="28" y="3" fill="rgba(186,223,219,0.7)"/>-->
+      <!--            <use xlink:href="#gentle-wave" x="28" y="5" fill="rgba(252,249,0,0.3)"/>-->
+      <!--            <use fill="rgba(167,215,197,0.9)" x="28" xlink:href="#gentle-wave" y="7"/>-->
+      <!--          </g>-->
+      <!--        </svg>-->
+      <!--      </div>-->
     </div>
     <div class="container" id="about">
       <div class="row align-items-center justify-content-center">
@@ -72,16 +72,31 @@
       </div>
     </div>
     <div class="container" id="contact">
-      <div class="row-tb-margin row">
-        <div class="col">
-          <form>
-            <label for="email">Email</label>
-            <input type="email" class="" id="email" placeholder="example@email.com">
+      <div class="row-tb-margin row align-items-center justify-content-center">
+        <div class="col-md-10">
+          <h2>Contact</h2>
+          <p>Have a question? Feel free to use the contact form below or email <a href="mailto:hello@masseyhacks.ca"
+                                                                                  class="expand-link">hello@masseyhacks.ca</a>.
+          </p>
+          <form id="contact-form" v-on:submit.prevent="submitContactForm">
+            <div class="form-group">
+              <label for="nameInput">Name</label>
+              <input type="text" class="form-control" id="nameInput" placeholder="John Smith" v-model="name">
+            </div>
+            <div class="form-group">
+              <label for="emailInput">Email address</label>
+              <input type="email" class="form-control" id="emailInput" placeholder="name@example.com" v-model="email">
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">Message</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" v-model="message"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary" style="font-size: 0.7em">Submit</button>
           </form>
         </div>
-
+      
       </div>
-
+    
     </div>
   </div>
 
@@ -90,10 +105,23 @@
 <script>
     import Logo from '~/components/Logo.vue'
     import Swal from 'sweetalert2'
+    import axios from '@nuxtjs/axios'
 
     export default {
         components: {
             Logo
+        },
+        data() {
+            return {
+                message: '',
+                name: '',
+                email: ''
+            }
+        },
+        methods: {
+            submitContactForm: function submitContactForm() {
+                Swal.fire('yay');
+            }
         }
     }
 </script>
