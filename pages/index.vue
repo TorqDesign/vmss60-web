4949c1516e9ff4588eb29e2a40ff88d7d3876f22
James Xu <36768789+jamesxu123@users.noreply.github.com>
2019-09-22 15:00:20 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,36 +8,6 @@
       <!--Content before waves-->
       <div class="inner-header flex">
       </div>
-      
-      <!--Waves Container-->
-      <!--      <div>-->
-      <!--        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"-->
-      <!--             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">-->
-      <!--          <defs>-->
-      <!--            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>-->
-      <!--          </defs>-->
-      <!--          <g class="parallax">-->
-      <!--            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(29,67,155,0.7)"/>-->
-      <!--            <use fill="rgba(245, 164, 66,0.5)" x="48" xlink:href="#gentle-wave" y="3"/>-->
-      <!--&lt;!&ndash;            <use fill="rgba(151,150,240,0.3)" x="48" xlink:href="#gentle-wave" y="5"/>&ndash;&gt;-->
-      <!--&lt;!&ndash;            <use fill="rgba(167,215,197,0.9)" x="48" xlink:href="#gentle-wave" y="7"/>&ndash;&gt;-->
-      <!--          </g>-->
-      <!--        </svg>-->
-      <!--      </div>-->
-      <!--      <div style="transform: rotate(180deg)">-->
-      <!--        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"-->
-      <!--             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">-->
-      <!--          <defs>-->
-      <!--            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>-->
-      <!--          </defs>-->
-      <!--          <g class="parallax">-->
-      <!--            <use xlink:href="#gentle-wave" x="28" y="0" fill="rgba(248,169,120,0.5)"/>-->
-      <!--            <use xlink:href="#gentle-wave" x="28" y="3" fill="rgba(186,223,219,0.7)"/>-->
-      <!--            <use xlink:href="#gentle-wave" x="28" y="5" fill="rgba(252,249,0,0.3)"/>-->
-      <!--            <use fill="rgba(167,215,197,0.9)" x="28" xlink:href="#gentle-wave" y="7"/>-->
-      <!--          </g>-->
-      <!--        </svg>-->
-      <!--      </div>-->
     </div>
     <div class="container" id="about">
       <div class="row align-items-center justify-content-center">
