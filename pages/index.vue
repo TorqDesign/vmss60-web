a2fd96a9cfe8f3bcc11bb85a5f15d58dc96b9157
James Xu <xujames007@gmail.com>
2019-09-28 14:00:41 -0400

    contact form and mailchimp

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,29 @@
         <h1>VMSS 60th Reunion</h1>
         <h6>October 9th & 10th / 2020</h6>
         <p>(To be held at Vincent Massey S.S. & Caboto Club)</p>
+        <!-- Begin Mailchimp Signup Form -->
+        <!-- Begin Mailchimp Signup Form -->
+        <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<!--        <style type="text/css">-->
+<!--          #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}-->
+<!--          /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.-->
+<!--             We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */-->
+<!--        </style>-->
+        <div id="mc_embed_signup">
+          <form action="https://gmail.us20.list-manage.com/subscribe/post?u=c259260007eff16ec9e630d39&amp;id=7fcb56d7d3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <label for="mce-EMAIL">Subscribe for updates!</label>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c259260007eff16ec9e630d39_7fcb56d7d3" tabindex="-1" value=""></div>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </div>
+          </form>
+        </div>
+  
+        <!--End mc_embed_signup-->
+  
+        <!--End mc_embed_signup-->
       </div>
       <!--Content before waves-->
       <div class="inner-header flex">
@@ -74,7 +97,7 @@
               <label for="exampleFormControlTextarea1">Message</label>
               <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" v-model="message"></textarea>
             </div>
-            <button type="submit" class="btn btn-primary" style="font-size: 0.7em">Submit</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
           </form>
         </div>
       
@@ -103,7 +126,24 @@
         },
         methods: {
             submitContactForm: function submitContactForm() {
-                Swal.fire('yay');
+                this.$axios.post('/email/contact-form', {
+                    message: this.message,
+                    name: this.name,
+                    email: this.email
+                }).then(data => {
+                    Swal.fire({
+                        title: 'Message Sent!',
+                        type: 'success',
+                        text: 'data'
+                    })
+                }).catch(error => {
+                    console.log(error);
+                    Swal.fire({
+                        title: 'There was an error!',
+                        type: 'error',
+                        text: error
+                    })
+                })
             }
         }
     }
