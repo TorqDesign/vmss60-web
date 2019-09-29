33cd8f404bdaf2770b842d48cc871e94e1851536
James Xu <xujames007@gmail.com>
2019-09-28 20:15:39 -0400

    fix desktop and mobile views

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,6 @@
         <p>(To be held at Vincent Massey S.S. & Caboto Club)</p>
         <!-- Begin Mailchimp Signup Form -->
         <!-- Begin Mailchimp Signup Form -->
-        <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
 <!--        <style type="text/css">-->
 <!--          #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}-->
 <!--          /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.-->
@@ -16,7 +15,7 @@
         <div id="mc_embed_signup">
           <form action="https://gmail.us20.list-manage.com/subscribe/post?u=c259260007eff16ec9e630d39&amp;id=7fcb56d7d3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
-              <label for="mce-EMAIL">Subscribe for updates!</label>
+              <label for="mce-EMAIL">Our site is constantly being updated, subscribe for updates!</label>
               <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
               <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
               <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c259260007eff16ec9e630d39_7fcb56d7d3" tabindex="-1" value=""></div>
