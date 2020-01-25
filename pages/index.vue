1e257c0720649ddaa21c0d69b4669fbffaad25f0
James Xu <xujames007@gmail.com>
2020-01-25 14:23:40 -0500

    remove events from main page

--- pages/index.vue
+++ pages/index.vue
@@ -66,25 +66,25 @@
             </div>
           </div>
         </div>-->
-        <div class="container" id="schedule" style="margin-top: 3em">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-md-10">
-                    <h2>Events</h2>
-                    <div class="schedule-box">
-                        <h6>Saturday Banquet</h6>
-                        <p>Fashion show of the decades featuring Massey alumni!</p>
-                    </div>
-                    <div class="schedule-box">
-                        <h6>Classic/Antique Car Show</h6>
-                        <p>View the beautiful collection of cars as you take a walk down memory lane.</p>
-                    </div>
-                    <div class="schedule-box">
-                        <h6>Battle of the Bands</h6>
-                        <p>Band names to be announced!</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!--        <div class="container" id="schedule" style="margin-top: 3em">-->
+<!--            <div class="row align-items-center justify-content-center">-->
+<!--                <div class="col-md-10">-->
+<!--                    <h2>Events</h2>-->
+<!--                    <div class="schedule-box">-->
+<!--                        <h6>Saturday Banquet</h6>-->
+<!--                        <p>Fashion show of the decades featuring Massey alumni!</p>-->
+<!--                    </div>-->
+<!--                    <div class="schedule-box">-->
+<!--                        <h6>Classic/Antique Car Show</h6>-->
+<!--                        <p>View the beautiful collection of cars as you take a walk down memory lane.</p>-->
+<!--                    </div>-->
+<!--                    <div class="schedule-box">-->
+<!--                        <h6>Battle of the Bands</h6>-->
+<!--                        <p>Band names to be announced!</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
         <div class="container" id="contact">
             <div class="row-tb-margin row align-items-center justify-content-center">
                 <div class="col-md-10">
