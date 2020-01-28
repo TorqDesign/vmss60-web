383b18d27d8f73942fe5e701bb0a7152c35d7b48
David Hui <opensource@davidhui.ca>
2020-01-27 23:00:38 -0500

    add navbar and contact
    Former-commit-id: 59f5a23925ebd055e594fa283075052c5e3238ea

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
     <div id="main">
         <!--        <Navbar></Navbar>-->
+        <div class="navbar">
+            <nuxt-link to="#main" :class="'navbar-item ' + getNavAddClass() + ' first'">
+                VMSS60
+            </nuxt-link>
+            <div class="navbar-line-container"><div :class="'navbar-line ' + getNavAddClass()"></div></div>
+            <nuxt-link to="#full-page-2" :class="'navbar-item ' + getNavAddClass()">
+                Hotels
+            </nuxt-link>
+            <div class="navbar-line-container"><div :class="'navbar-line ' + getNavAddClass()"></div></div>
+            <nuxt-link to="#full-page-3" :class="'navbar-item ' + getNavAddClass()">
+                Events
+            </nuxt-link>
+            <div class="navbar-line-container"><div :class="'navbar-line ' + getNavAddClass()"></div></div>
+            <nuxt-link to="#full-page-4" :class="'navbar-item ' + getNavAddClass()">
+                Contact
+            </nuxt-link>
+        </div>
         
         <div class="hero-background full-page">
             <div class="hero">
@@ -19,7 +36,7 @@
             </div>
         </div>
         
-        <div class="full-page" id="full-page-2">
+        <div class="full-page" id="full-page-2"  v-waypoint="{ active: true, callback: toggleNavbar, options: intersectionOptions }">
             <img src="../assets/images/vmss60-hotelArtboard1.png" alt="blob" style="">
             <div id="full-page-2-text">
                 <h1>Hotels.</h1>
@@ -40,6 +57,46 @@
                     cras sed felis eget velit aliquet.</p>
                 <nuxt-link to="/events" class="section-action-button">Check them out →</nuxt-link>
             </div>
+        </div>
+
+        <div class="full-page" id="full-page-4"  v-waypoint="{ active: true, callback: toggleNavbar, options: intersectionOptions }">
+            <div id="full-page-4-text">
+                <h1>Questions?</h1>
+                <p>Feel free to use the contact form below or email <a
+                        href="mailto:masseymustangs2020@gmail.com"
+                        class="expand-link">masseymustangs2020@gmail.com</a>.
+                </p>
+                <form id="contact-form" v-on:submit.prevent="recaptcha">
+                    <div class="form-group">
+                        <label for="nameInput">Name</label>
+                        <input type="text" class="form-control" id="nameInput" placeholder="John Smith"
+                               v-model="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="emailInput">Email address</label>
+                        <input type="email" class="form-control" id="emailInput" placeholder="name@example.com"
+                               v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Message</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                  v-model="message"></textarea>
+                    </div>
+                    <p>This site is protected by reCAPTCHA and the Google
+                    <a class="expand-link" href="https://policies.google.com/privacy">Privacy Policy</a> and
+                    <a class="expand-link" href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                </form>
+            </div>
+            <div class="vmss60-footer">
+                <div class="quarter-circle-bottom-right">
+                </div>
+                <div class="vmss60-footer vmss60-footer-text">
+                    <p>Copyright &copy; 2020 Vincent Massey Secondary School.<br>Design by <a class="expand-link" href="https://davidhui.ca" target="_blank">David Hui</a> and <a class="expand-link" href="https://jamesxu.ca" target="_blank">James Xu</a></p>
+                </div>
+            </div>
+
         </div>
         
         <!--        <div id="mc_embed_signup">-->
@@ -110,7 +167,9 @@
             return {
                 message: '',
                 name: '',
-                email: ''
+                email: '',
+                intersectionOptions: {threshold: 0.95},
+                lightBack: false // First toggle is on page load, so technically the lightBack is set to false
             }
         },
         methods: {
@@ -147,6 +206,13 @@
                         this.submitContactForm(token)
                     })
                 }
+            },
+            toggleNavbar: function toggleNavbar(){
+                console.log("hello");
+              this.lightBack = !this.lightBack;
+            },
+            getNavAddClass: function getNavAddClass() {
+                return this.lightBack ? "black" : "";
             }
         }
     }
