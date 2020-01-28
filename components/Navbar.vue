9cf16c92df780901d28fae452c52a41a572e5859
David Hui <opensource@davidhui.ca>
2020-01-28 15:36:24 -0500

    navbar to component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,28 +1,75 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <nuxt-link class="navbar-brand" to="/">VMSS60</nuxt-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="/">Home</nuxt-link>
-                </li>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="/events">Events</nuxt-link>
-                </li>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="/hotels">Hotels</nuxt-link>
-                </li>
-            </ul>
+    <div>
+        <div :class="'vmss60-navbar-icon mobile '  + getNavAddClass()" @click="toggleMobileNavbar()"></div>
+        <!--        <div class="navbar-icon mobile black" @click="toggleMobileNavbar()"></div>-->
+        <div class="vmss60-navbar mobile">
+            <div class="vmss60-navbar-item-container d-none d-lg-flex">
+                <button :class="'vmss60-navbar-item ' + getNavAddClass() + ' first'" @click="$emit('navTo','home')">
+                    VMSS60
+                </button>
+                <div class="vmss60-navbar-line-container"><div :class="'vmss60-navbar-line ' + getNavAddClass()"></div></div>
+                <button :class="'vmss60-navbar-item ' + getNavAddClass()" @click="$emit('navTo','hotels')">
+                    Hotels
+                </button>
+                <div class="vmss60-navbar-line-container"><div :class="'vmss60-navbar-line ' + getNavAddClass()"></div></div>
+                <button :class="'vmss60-navbar-item ' + getNavAddClass()" @click="$emit('navTo','events')">
+                    Events
+                </button>
+                <div class="vmss60-navbar-line-container"><div :class="'vmss60-navbar-line ' + getNavAddClass()"></div></div>
+                <button :class="'vmss60-navbar-item ' + getNavAddClass()" @click="$emit('navTo','contact')">
+                    Contact
+                </button>
+            </div>
+            <div :class="'vmss60-navbar-item-container d-block d-lg-none' + (mobileNavOpen ? ' open' : '')">
+                <ul>
+                    <li>
+                        <button class="vmss60-navbar-item mobile" @click="$emit('navTo','home')">
+                            VMSS60
+                        </button>
+                    </li>
+                    <li>
+                        <button class="vmss60-navbar-item mobile" @click="$emit('navTo','hotels')">
+                            Hotels
+                        </button>
+                    </li>
+                    <li>
+                        <button class="vmss60-navbar-item mobile" @click="$emit('navTo','events')">
+                            Events
+                        </button>
+                    </li>
+                    <li>
+                        <button class="vmss60-navbar-item mobile" @click="$emit('navTo','contact')">
+                            Contact
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </nav>
+        <div :class="'navbar-overlay' + (mobileNavOpen ? ' open' : '')"></div>
+    </div>
+
 </template>
 
 <script>
     export default {
-        name: "Navbar"
+        name: "Navbar",
+        data() {
+            return {
+                mobileNavOpen: false,
+                lightBack: true, // First toggle is on page load, so technically the lightBack is set to false
+            }
+        },
+        methods: {
+            toggleNavbar: function toggleNavbar() {
+                this.lightBack = !this.lightBack;
+            },
+            getNavAddClass: function getNavAddClass() {
+                return this.mobileNavOpen ? "" : this.lightBack ? "black" : "";
+            },
+            toggleMobileNavbar: function toggleMobileNavbar() {
+                this.mobileNavOpen = !this.mobileNavOpen;
+            }
+        }
     }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,7 @@
 <template>
     <div id="main">
-        <!--        <Navbar></Navbar>-->
-        <div :class="'vmss60-navbar-icon mobile '  + getNavAddClass()" @click="toggleMobileNavbar()"></div>
-        <!--        <div class="navbar-icon mobile black" @click="toggleMobileNavbar()"></div>-->
-        <div class="vmss60-navbar mobile">
-            <div class="vmss60-navbar-item-container d-none d-lg-flex">
-                <button :class="'vmss60-navbar-item ' + getNavAddClass() + ' first'"
-                        @click="$refs.fullpage.api.moveTo('home')">
-                    VMSS60
-                </button>
-                <div class="vmss60-navbar-line-container">
-                    <div :class="'vmss60-navbar-line ' + getNavAddClass()"></div>
-                </div>
-                <button :class="'vmss60-navbar-item ' + getNavAddClass()" @click="$refs.fullpage.api.moveTo('hotels')">
-                    Hotels
-                </button>
-                <div class="vmss60-navbar-line-container">
-                    <div :class="'vmss60-navbar-line ' + getNavAddClass()"></div>
-                </div>
-                <button :class="'vmss60-navbar-item ' + getNavAddClass()" @click="$refs.fullpage.api.moveTo('events')">
-                    Events
-                </button>
-                <div class="vmss60-navbar-line-container">
-                    <div :class="'vmss60-navbar-line ' + getNavAddClass()"></div>
-                </div>
-                <button :class="'vmss60-navbar-item ' + getNavAddClass()" @click="$refs.fullpage.api.moveTo('contact')">
-                    Contact
-                </button>
-            </div>
-            <div :class="'vmss60-navbar-item-container d-block d-lg-none' + (mobileNavOpen ? ' open' : '')">
-                <ul>
-                    <li>
-                        <button class="vmss60-navbar-item mobile" @click="$refs.fullpage.api.moveTo('home')">
-                            VMSS60
-                        </button>
-                    </li>
-                    <li>
-                        <button class="vmss60-navbar-item mobile" @click="$refs.fullpage.api.moveTo('hotels')">
-                            Hotels
-                        </button>
-                    </li>
-                    <li>
-                        <button class="vmss60-navbar-item mobile" @click="$refs.fullpage.api.moveTo('events')">
-                            Events
-                        </button>
-                    </li>
-                    <li>
-                        <button class="vmss60-navbar-item mobile" @click="$refs.fullpage.api.moveTo('contact')">
-                            Contact
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div :class="'navbar-overlay' + (mobileNavOpen ? ' open' : '')"></div>
+        <Navbar ref="navbar" @navTo="navTo"></Navbar>
+
         <full-page ref="fullpage" id="fullpage" :options="options">
             <div class="hero-background full-page section" id="top" data-anchor="home">
                 <div class="hero">
@@ -238,8 +186,6 @@
                 email: '',
                 counter: -1,
                 intersectionOptions: {threshold: 0.95},
-                mobileNavOpen: false,
-                lightBack: true, // First toggle is on page load, so technically the lightBack is set to false
                 time: event - now,
                 options: {
                     licenseKey: 'dddddddd-dddddddd-dddddddd-dddddddd'
@@ -289,13 +235,10 @@
                 }
             },
             toggleNavbar: function toggleNavbar() {
-                this.lightBack = !this.lightBack;
+                this.$refs.navbar.toggleNavbar();
             },
-            getNavAddClass: function getNavAddClass() {
-                return this.mobileNavOpen ? "" : this.lightBack ? "black" : "";
-            },
-            toggleMobileNavbar: function toggleMobileNavbar() {
-                this.mobileNavOpen = !this.mobileNavOpen;
+            navTo: function navTo(location){
+                this.$refs.fullpage.api.moveTo(location);
             }
         }
     }
