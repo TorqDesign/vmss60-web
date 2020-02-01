fde0855bf725d6cd5f5da7438dd187f6bb16bb08
James Xu <xujames007@gmail.com>
2020-01-31 21:02:15 -0500

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,42 @@ export default {
         ** You can extend webpack config here
         */
         extend(config, ctx) {
-        }
+        },
+        // babel: {
+        //     presets: function ({isServer}, [preset, options]) {
+        //
+        //         const r = [
+        //             [
+        //                 preset, {
+        //                 buildTarget: isServer ? 'server' : 'client',
+        //                 ...options
+        //             }]
+        //             // [ Other presets ]
+        //         ];
+        //
+        //         r[0][1].targets = {
+        //             "browsers": ["> 1%", "last 2 versions"],
+        //             ie: 11
+        //         };
+        //
+        //         r[0][1].polyfills = [
+        //             'es6.array.iterator',
+        //             'es6.promise',
+        //             'es6.object.assign',
+        //             'es6.symbol',
+        //             'es6.array.find',
+        //             'es6.array.from',
+        //             'es7.promise.finally',
+        //             'es7.object.entries',
+        //         ];
+        //
+        //         return r;
+        //     },
+        //
+        //     plugins: [
+        //         ['@babel/plugin-transform-runtime']
+        //     ]
+        // }
     },
     /*
   ** Router configuration
