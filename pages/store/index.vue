b6631f8dfeefd7e123d291ad3243a085617f26e5
James Xu <xujames007@gmail.com>
2020-02-26 12:11:54 -0500

    revert

--- pages/store/index.vue
+++ pages/store/index.vue
@@ -78,9 +78,9 @@
                 ]
             }
         },
-        async mounted() {
+        async beforeMount() {
             const token = await this.$auth.getToken('auth0');
-            const res = await this.$axios.get('/allProducts', {
+            const res = await this.$axios.get('/api/allProducts', {
                 headers: {
                     Authorization: token    // send the access token through the 'Authorization' header
                 }
@@ -109,7 +109,7 @@
                 }
                 console.log(cartParsed);
                 // Use Axios to make a call to the API
-                this.$axios.post("/createCheckoutSession", {
+                this.$axios.post("/api/createCheckoutSession", {
                     headers: {
                         Authorization: token    // send the access token through the 'Authorization' header
                     },
