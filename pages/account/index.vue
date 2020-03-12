977d063997bdd04c8642a63beef9c7ab0c98d515
James Xu <xujames007@gmail.com>
2020-03-11 21:20:49 -0400

    oops use error instead of e

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -174,7 +174,8 @@
                 catch(e){
                     let errorBody = '<p>Unable to submit the form due to the following reason(s):</p><ul class="list-unstyled">';
                     for(let error of e){
-                        errorBody += '<li><strong>' + e.message + '</strong></li>'
+                        console.log(error)
+                        errorBody += '<li><strong>' + error.message + '</strong></li>'
                     }
                     errorBody += '</ul>';
 
